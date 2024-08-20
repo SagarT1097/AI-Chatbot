20a18098f471e45ac5e12d4f460d03bb63ea0357
Sagar Tambrahalli <sagar.uu97@gmail.com>
2024-08-20 14:45:07 +0200

    changed testiing

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -4,7 +4,9 @@ const ChatHeader = () => {
   return (
     <div className="w=full flex gap-3 justify-start items-center text-zinc-800">
       <div className="flex flex-col items-start text-sm">
-        <p className="text-xs">Chat with me hello hellp</p>
+        <p className="text-xs">
+          Second commit creating pull request test hellow whattt test
+        </p>
         <div className="flex gap-1.5 items-center">
           <p className="w-2 h-2 rounded-full bg-green-500" />
           <p className="font-medium">Our OpenAI Integrated Chatbot</p>
